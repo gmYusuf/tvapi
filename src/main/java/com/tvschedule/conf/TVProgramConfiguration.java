d0999e0ec5a481a2cfce82dc0fc07921f507a5eb
y00429754 <yusufaltungoldtr@hotmail.com>
2020-01-31 16:59:40 +0300

    pom.xml updated

--- src/main/java/com/tvschedule/conf/TVProgramConfiguration.java
+++ src/main/java/com/tvschedule/conf/TVProgramConfiguration.java
@@ -4,13 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
 public class TVProgramConfiguration extends Configuration {
-    private String writerName;
-    @JsonProperty
-    public String getWriterName() {
-        return writerName;
-    }
-    @JsonProperty
-    public void setWriterName(String writerName) {
-        this.writerName = writerName;
-    }
+
 }
